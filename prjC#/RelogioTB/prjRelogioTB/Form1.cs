a1800f8123bacd9f52b2f044940195862b419a73
Matheus Maximos Almeida <matheusmaximos452@gmail.com>
2022-04-12 22:41:46 -0300

    edited prjRelogio

--- prjC#/RelogioTB/prjRelogioTB/Form1.cs
+++ prjC#/RelogioTB/prjRelogioTB/Form1.cs
@@ -18,7 +18,7 @@ namespace prjRelogioTB
         }
 
         string caminho = Environment.CurrentDirectory + "\\fundo.png";
-        Image fundo;
+        Bitmap fundo;
         Graphics g;
         int hora;
         int min;
@@ -32,12 +32,9 @@ namespace prjRelogioTB
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            fundo = Image.FromFile(caminho);
+            fundo = new Bitmap(pbRelogio.Width, pbRelogio.Height);
             pbRelogio.Image = fundo;
-            g = pbRelogio.CreateGraphics();
-            pbRelogio.Height = fundo.Height;
-
-
+            g = Graphics.FromImage(fundo);
         }
 
         private void relogio_Tick(object sender, EventArgs e)
@@ -46,8 +43,72 @@ namespace prjRelogioTB
             hora = bios.Hour;
             min = bios.Minute;
             seg = bios.Second;
+            DesenharFundo();
             DesenharPonteirosSegundo();
+            DesenharPonteiroMinuto();
+            DesenharPonteiroHora();
+            DesenharCentro();
+            DesenharDigital();
+            pbRelogio.CreateGraphics().DrawImage(fundo, 0, 0);
 
+        }
+
+        private void DesenharDigital()
+        {
+            SolidBrush corSolida = new SolidBrush(Color.Black);
+            g.DrawString(DateTime.Now.ToLongTimeString(), new Font("Arial", 12),
+                corSolida, 120, 180);
+        }
+
+        private void DesenharFundo()
+        {
+            g.DrawImage(Image.FromFile(caminho), 0, 0, pbRelogio.Width, pbRelogio.Height);
+        }
+
+        private void DesenharCentro()
+        {
+            SolidBrush corSolida = new SolidBrush(Color.Red);
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            g.FillEllipse(corSolida, cx - 10, cy - 10, 20, 20);
+            corSolida.Color = Color.White;
+            g.FillEllipse(corSolida, cx - 8, cy - 8, 16, 16);
+        }
+
+        private void DesenharPonteiroHora()
+        {
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            int raio = 90;
+            if (hora > 12) hora = hora - 12;
+            double angulo = -90 + (hora * 30);
+            angulo += (min / 12) * 6;
+            Pen caneta = new Pen(Color.Transparent, 4);
+            caneta.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            g.DrawLine(caneta, cx, cy, cx + xhora, cy + yhora);
+            double rad = Math.PI * angulo / 180;
+            xhora = (int)(raio * Math.Cos(rad));
+            yhora = (int)(raio * Math.Sin(rad));
+            caneta.Color = Color.Black;
+            g.DrawLine(caneta, cx, cy, cx + xhora,
+              cy + yhora);
+        }
+
+        private void DesenharPonteiroMinuto()
+        {
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            int raio = 90;
+            double angulo = -90 + (min * 6);
+            Pen caneta = new Pen(Color.Transparent, 4);
+            caneta.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            g.DrawLine(caneta, cx, cy, cx + xmin, cy + ymin);
+            double rad = Math.PI * angulo / 180;
+            xmin = (int)(raio * Math.Cos(rad));
+            ymin = (int)(raio * Math.Sin(rad));
+            caneta.Color = Color.Red;
+            g.DrawLine(caneta, cx, cy, cx + xmin,
+              cy + ymin);
         }
 
         private void DesenharPonteirosSegundo()
@@ -56,7 +117,8 @@ namespace prjRelogioTB
             int cy = pbRelogio.Height / 2;
             int raio = 110;
             double angulo = -90 + (seg * 6);
-            Pen caneta = new Pen(Color.White, 4);
+            Pen caneta = new Pen(Color.Transparent, 4);
+            caneta.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
             g.DrawLine(caneta, cx, cy, cx + xseg, cy + yseg);
             double rad = Math.PI * angulo / 180;
             xseg = (int)(raio * Math.Cos(rad));
