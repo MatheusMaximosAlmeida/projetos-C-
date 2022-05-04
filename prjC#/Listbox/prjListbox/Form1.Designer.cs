e263d0af6707adb8a731dd0e6085856618fb43b4
Matheus Maximos Almeida <matheusmaximos452@gmail.com>
2022-05-04 20:17:01 -0300

    update prjListBBox
    Até onde o aquiles passou.

---
+++ prjC#/Listbox/prjListbox/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace prjListbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbResposta = new System.Windows.Forms.Label();
+            this.btnMaior = new System.Windows.Forms.Button();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peso:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(12, 55);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(164, 22);
+            this.txtPeso.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(182, 37);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(113, 57);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(309, 38);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(113, 57);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(436, 37);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(113, 57);
+            this.btnRemover.TabIndex = 2;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 16;
+            this.lista.Location = new System.Drawing.Point(12, 160);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(362, 276);
+            this.lista.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Seleciona a unidade";
+            // 
+            // lbResposta
+            // 
+            this.lbResposta.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbResposta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbResposta.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResposta.Location = new System.Drawing.Point(380, 160);
+            this.lbResposta.Name = "lbResposta";
+            this.lbResposta.Size = new System.Drawing.Size(168, 50);
+            this.lbResposta.TabIndex = 0;
+            this.lbResposta.Text = "Resposta";
+            this.lbResposta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnMaior
+            // 
+            this.btnMaior.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaior.Location = new System.Drawing.Point(379, 387);
+            this.btnMaior.Name = "btnMaior";
+            this.btnMaior.Size = new System.Drawing.Size(168, 49);
+            this.btnMaior.TabIndex = 2;
+            this.btnMaior.Text = "Maior";
+            this.btnMaior.UseVisualStyleBackColor = true;
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(379, 229);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(168, 49);
+            this.btnSomar.TabIndex = 2;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedia.Location = new System.Drawing.Point(381, 308);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(168, 49);
+            this.btnMedia.TabIndex = 2;
+            this.btnMedia.Text = "Média";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 540);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.btnMaior);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lbResposta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbResposta;
+        private System.Windows.Forms.Button btnMaior;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnMedia;
+    }
+}
+
---
+++ prjC#/Listbox/prjListbox/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjListbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double peso = Convert.ToDouble(txtPeso.Text);
+                lista.Items.Add(peso); // Adiciona valor na lista
+                txtPeso.Focus(); // Posiciona o mouse na caixa txtPeso
+                txtPeso.Text = "0";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro ao inseriro peso na lista");
+            }
+
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+
+            if (lista.Items.Count > 0)
+            {
+                lista.Items.Remove(lista.SelectedItem);
+            }
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (lista.Items.Count > 0)
+            {
+                double peso = Convert.ToDouble(txtPeso.Text);
+                lista.Items[lista.SelectedIndex] = peso;
+            }
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double soma = 0;
+            for (int i = 0; i < lista.Items.Count; i++)
+            {
+                soma += Convert.ToDouble(lista.Items[i]);
+            }
+            lbResposta.Text = "A soma é = " + soma.ToString();
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            double soma = 0;
+            double media;
+            for (int i = 0; i < lista.Items.Count; i++)
+            {
+                soma += Convert.ToDouble(lista.Items[i]);
+            }
+            media = soma / lista.Items.Count;
+            lbResposta.Text = "A média é = " + media.ToString();
+        }
+
+    }
+}
